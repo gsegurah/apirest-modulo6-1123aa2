773732d97a2f28ee5c4b12e0a658aeb91ce0309d
Giovanni Segura Hernandez <gsegurah@msn.com>
2025-06-18 17:48:57 -0500

    se creo el archivo index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express =require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const vehiculosRoute=require('./routes/vehiculos.rutas');//
+app.use('/servicios', vehiculosRoute);
+mongoose.connect('mongodb+srv://gseguraher:usNrHo15g1mbHBGM@cluster0.jicfvuv.mongodb.net/?retryWrites=true&w=majority&appName=cluster0',
+{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+const connection = mongoose.connection; // creo conexion a la base de datos
+connection.once('open', () => {
+    console.log('MongoDB conexion de base de datos establecida');
+});
+
+app.listen(3000);// puerto de escucha
