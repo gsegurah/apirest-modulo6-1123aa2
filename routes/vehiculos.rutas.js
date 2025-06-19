7e22ed9f3f7f773922a039f5cf86f76d8ecbbff8
Giovanni Segura Hernandez <gsegurah@msn.com>
2025-06-18 20:39:26 -0500

    se cambio vehiculos.rutas

--- routes/vehiculos.rutas.js
+++ routes/vehiculos.rutas.js
@@ -35,7 +35,7 @@ router.post('/', async (req, res) => { // crear un post
 router.patch('/:postId', async (req, res) => { // actualizar un post
     try {
         const updatedPost = await Vehiculos.updateOne(
-            { _id: req.params.postId }, { $set: { title:   placa: req.body.placa,
+            { _id: req.params.postId }, { $set: { placa: req.body.placa,
   marca: req.body.marca,
   linea: req.body.linea,
   modelo: req.body.modelo
